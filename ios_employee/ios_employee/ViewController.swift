9ac24adfb49cae4e6b8e3d18db80e036797f1465
Braelyn Chen <braelynchen@braelyns-mbp.home>
2019-02-28 01:01:14 -0800

    employee

---
+++ ios_employee/ios_employee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios_employee
+//
+//  Created by Braelyn Chen on 2/27/19.
+//  Copyright © 2019 Braelyn Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
